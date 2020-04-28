2a2ca95c58aa014e4aa4cd44bc0a14a2c4cae4a8
王權 <timowang@wangquande-MacBook-Pro.local>
2020-04-28 22:04:12 +0800

    sequelize connection ready

---
+++ src/db/sequelize.js
@@ -0,0 +1,22 @@
+import Sequelize from 'sequelize';
+
+const {
+    MYSQL_USERNAME,
+    MYSQL_ROOT_PASSWORD,
+    MYSQL_HOSTNAME,
+    MYSQL_PORT,
+} = process.env;
+
+const sequelize = new Sequelize(`mysql://${MYSQL_USERNAME}:${MYSQL_ROOT_PASSWORD}@${MYSQL_HOSTNAME}:${MYSQL_PORT}`);
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch((err) => {
+        console.error('Unable to connect to the database:', err);
+        process.exit(1);
+    });
+
+export default sequelize;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import './db/sequelize';
 
 const app = express();
 
