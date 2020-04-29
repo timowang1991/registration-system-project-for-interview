d2a4055fae0920c6908fff7ea722f68b0daea670
王權 <timowang@wangquande-MacBook-Pro.local>
2020-04-29 19:21:56 +0800

    update user model

--- src/models/User.js
+++ src/models/User.js
@@ -2,13 +2,23 @@ import Sequelize from 'sequelize';
 import sequelize from '../db/sequelize';
 
 const User = sequelize.define('user', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
     email: {
         type: Sequelize.STRING,
-        unique: true,
+        unique: 'email_type',
         allowNull: false,
     },
     password: {
         type: Sequelize.STRING,
+    },
+    type: {
+        type: Sequelize.ENUM('email', 'google', 'facebook'),
+        unique: 'email_type',
+        allowNull: false,
     },
     name: {
         type: Sequelize.STRING,
