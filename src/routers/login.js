c3d0e1d82cde1d608c40561c5b0e3688515b1062
王權 <timowang@wangquande-MacBook-Pro.local>
2020-04-30 23:08:46 +0800

    add social login in /login

--- src/routers/login.js
+++ src/routers/login.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import passport from '../auth/passport';
 
 const router = express.Router();
+
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+router.get('/facebook', passport.authenticate('facebook'));
 
 export default router;
