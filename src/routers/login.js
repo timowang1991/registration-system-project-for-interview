4b4299a7a0b01d477cdf905f6047d62cd29717cf
王權 <timowang@wangquande-MacBook-Pro.local>
2020-05-01 00:17:17 +0800

    login email endpoint ready

--- src/routers/login.js
+++ src/routers/login.js
@@ -1,7 +1,38 @@
 import express from 'express';
 import passport from '../auth/passport';
+import User from '../models/User';
 
 const router = express.Router();
+
+router.post('/', async (req, res) => {
+    const { email, password } = req.body;
+
+    let user;
+
+    try {
+        user = await User.findOne({
+            where: {
+                email,
+                password,
+                provider: 'email',
+            },
+        });
+    } catch (error) {
+        return res.status(500).send({
+            error: `unable to find user: ${error.message}`,
+        });
+    }
+
+    if (!user) {
+        return res.status(400).send({
+            error: 'email or password incorrect',
+        });
+    }
+
+    const token = user.generateAuthToken();
+
+    return res.header({ token }).send('email login complete');
+});
 
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
