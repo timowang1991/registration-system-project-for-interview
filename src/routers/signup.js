c4850d9ceb34402a538daffbdbfb56438c779e04
王權 <timowang@wangquande-MacBook-Pro.local>
2020-05-01 00:08:10 +0800

    add signup email

--- src/routers/signup.js
+++ src/routers/signup.js
@@ -1,13 +1,36 @@
 import express from 'express';
+import generator from 'generate-password';
 import passport from '../auth/passport';
 import signupValidator from '../validation/signup-validator';
+import User from '../models/User';
 
 const router = express.Router();
 
-router.post('/', signupValidator, (req, res) => {
-    // const { email, password, name } = req.body;
+router.post('/', signupValidator, async (req, res) => {
+    const { email, password, name } = req.body;
 
-    res.send('ok');
+    let user;
+
+    try {
+        user = await User.create({
+            email,
+            password,
+            name,
+            provider: 'email',
+            coupon: generator.generate({
+                length: 6,
+                numbers: true,
+            }).toUpperCase(),
+        });
+    } catch (error) {
+        return res.status(500).send({
+            error: `unable to create User: ${error.message}`,
+        });
+    }
+
+    const token = user.generateAuthToken();
+
+    return res.header({ token }).send('email signup complete');
 });
 
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
--- src/validation/signup-validator.js
+++ src/validation/signup-validator.js
@@ -1,3 +1,5 @@
+import User from '../models/User';
+
 function validateEmailFormat(email) {
     // eslint-disable-next-line
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -5,13 +7,24 @@ function validateEmailFormat(email) {
     return re.test(email);
 }
 
-function validateEmail(req, res, next) {
+async function validateEmail(req, res, next) {
     const { email } = req.body;
     if (!validateEmailFormat(email)) {
         return res.status(400).send({
             error: 'email format error',
         });
     }
+
+    const user = await User.findOne({
+        where: { email, provider: 'email' },
+    });
+
+    if (user) {
+        return res.status(409).send({
+            error: 'email already exists',
+        });
+    }
+
     return next();
 }
 
